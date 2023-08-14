d1c1b6f8ac932d8aee559b85c5742cf641097adc
Abhas Chatterjee <abhas2201@gmail.com>
2023-08-14 10:19:30 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section id="home" aria-label="home">
+      <main id="body">
+        <div className="img-container">
+          <Image
+            src="/assets/images/backsoon.jpg"
+            alt="backsoon"
+            fill
+            sizes="100%"
+          ></Image>
+        </div>
+        <div className="back-soon-text">
+          <h2>Website under maintainance</h2>
+          <div className="span-bank">
+            <span>For support and queries, please contact:</span>
+            <a href="mailto:contacts@quantumshift.in">
+              contacts@quantumshift.in
+            </a>
+            <a href="mailto:ceo@quantumshift.in">ceo@quantumshift.in</a>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
+}
