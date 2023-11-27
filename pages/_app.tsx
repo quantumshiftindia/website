abfade014a1a484b09972ca8848e88ce510b5a88
Abhas <abhas2201@gmail.com>
2023-11-27 23:41:47 +0530

    added new photo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,16 @@ import {default as Footer} from "@/components/footer";
 import {default as Header} from "@/components/header";
 import "@/styles/styles.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>QuantumShift</title>
+        <meta name="description" content="QuantumShift"></meta>
+        <meta name="author" content="Abhas Chatterjee"></meta>
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <title>QuantumShift</title>
+      <Head/>
       <body>
         <Main />
         <NextScript />
